9fb2c1ab65c9f8012965bf3ff05f246c36a1c8f9
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-08 15:47:56 +1000

    Add a notice for Php 5.6

--- modules/v8js/manifests/init.pp
+++ modules/v8js/manifests/init.pp
@@ -2,12 +2,16 @@
 class v8js(
  	$config
 ) {
-	include apt
+	if ( $config[php] == 5.6 ) {
+		alert('V8JS requires Php 7.0 or higher. This extension will not be installed.')
+	} else {
+		include apt
 
-	class { 'v8js::extension':
-		config  => $config,
-		require => [
-			Class['apt'],
-		],
+		class { 'v8js::extension':
+			config  => $config,
+			require => [
+				Class['apt'],
+			],
+		}
 	}
 }
